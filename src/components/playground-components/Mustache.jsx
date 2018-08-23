f1dd7d4741d8de9079c6246aa9dd9d9bc01d8bb0
tbhesswebber <tbhesswebber@gmail.com>
2018-08-22 23:20:21 -0400

    [WIP] Adds keyframs and pseudoclass  example.

--- src/components/playground-components/Mustache.jsx
+++ src/components/playground-components/Mustache.jsx
@@ -1,87 +1,59 @@
-import React from 'react';
 import styled from 'styled-components';
-
-// const Left = styled.div`
-//   border-radius: 50%;
-//   height: 10vmin;
-//   width: 10vmin;
-//   background-color: #583713;
-
-//   ::before {
-//     display: block;
-//     content: '';
-//     height: 10vmin;
-//     width: 10vmin;
-//     background-color: #583713;
-//     background-color: peru;
-//     border-radius: 0 50% 50% 0;
-//     position: relative;
-//     left: -50%;
-//   }
-// `;
 
 const BeardColor = '#583713';
 
-const Left = styled.div`
-  width: 60px;
-  height: 60px;
+const Mustache = styled.div`
+  background-color: ${BeardColor}
+  width: 20vmin;
+  height: 20vmin;
+  position: relative;
+  left: -10vmin;
   border-radius: 50%;
-  box-shadow: 50px 80px 0 0 ${BeardColor}, 100px 80px 0 0 ${BeardColor};
-
-  ::after {
-    content: '';
-    border: 0 solid transparent;
-    border-bottom: 60px solid ${BeardColor};
-    border-radius: 0 0 0 100%;
-    width: 70px;
-    height: 40px;
-    left: 10px;
-    top: 40px;
-    transform-origin: right 70px;
-    transform: rotate(-40deg);
-  }
+  box-shadow: 19vmin 0 0 0 ${BeardColor};
 
   ::before {
     content: '';
-    border: 0 solid transparent;
-    border-bottom: 60px solid ${BeardColor};
-    border-radius: 0 0 100% 0;
-    width: 70px;
-    height: 40px;
-    left: 130px;
-    top: 40px;
-    transform-origin: left 70px;
-    transform: rotate(40deg);
+    position: absolute;
+    left: -26vmin;
+    width: 36vmin;
+    height: 20vmin;
+    border-radius: 100% 0 0 1%;
+    background-color: ${BeardColor};
+    transform-origin: right center;
+    transform: rotate(-2deg)
+  }
+
+  ::after {
+    content: '';
+    position: absolute;
+    left: 29vmin;
+    width: 36vmin;
+    height: 20vmin;
+    border-radius: 0 100% 1% 0;
+    background-color: ${BeardColor};
+    transform-origin: left center;
+    transform: rotate(2deg)
+  }
+
+  :hover::before {
+    animation: wiggle-left 300ms 2 both alternate ease-in-out;
+  }
+
+  :hover::after {
+    animation: wiggle-right 300ms 2 both alternate ease-in-out;
+  }
+
+  @keyframes wiggle-left {
+    0% {transform: rotate(-2deg)}
+    60% {transform: rotate(-10deg)}
+    100% {transform: rotate(-5deg)}
+  }
+
+  @keyframes wiggle-right {
+    0% {transform: rotate(2deg)}
+    60% {transform: rotate(10deg)}
+    100% {transform: rotate(5deg)}
   }
 `;
 
-const Mustache = () => <Left />;
-
 export default Mustache;
-
-/**
- * border: 0 solid transparent;
-    border-bottom: 6px solid #333333;
-    border-radius: 0 0 0 100%;
-    width: 7px;
-    height: 4px;
-    left: 1px;
-    top: 4px;
-    transform-origin: right 7px;
-    transform: rotate(-40deg);
-
-    border: 0 solid transparent;
-    border-bottom: 6px solid #333333;
-    border-radius: 0 0 100% 0;
-    width: 7px;
-    height: 4px;
-    left: 13px;
-    top: 4px;
-    transform-origin: left 7px;
-    transform: rotate(40deg);
-
-    width: 6px;
-    height: 6px;
-    border-radius: 50%;
-    box-shadow: 5px 8px 0 0 #333333, 10px 8px 0 0 #333333;
- */
